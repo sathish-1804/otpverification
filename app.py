fdbd502862ce022af4546c9a9718167d94ff2c0d
Sathish <107974739+sathish-1804@users.noreply.github.com>
2023-08-03 21:26:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from email.mime.text import MIMEText
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+otp_secrets={}
 
 def send_otp_email(recipient_email, otp):
 
@@ -29,6 +30,7 @@ def send_otp_email(recipient_email, otp):
     except smtplib.SMTPException as e:
         return jsonify({'message':f'An error occurred while sending the email: {e}'})
 
+
 @app.route('/send-otp', methods=['POST'])
 def send_otp():
     data = request.get_json()
@@ -37,13 +39,17 @@ def send_otp():
     if not recipient_email:
         return jsonify({'error': 'Email is missing'}), 400
 
-    # Generate the OTP using pyotp with counter-based HOTP
-    otp_secret = pyotp.random_base32()
-    hotp = pyotp.HOTP(otp_secret)
-    otp_counter = 0  # Set the initial counter value
+    # Check if OTP secret for the email already exists
+    if recipient_email not in otp_secrets:
+        # Generate the OTP secret for the recipient email if it doesn't exist
+        otp_secret = pyotp.random_base32()
+        otp_secrets[recipient_email] = otp_secret
+    else:
+        otp_secret = otp_secrets[recipient_email]
 
-    # Send the OTP via email
-    otp = hotp.at(otp_counter)
+    # Use TOTP instead of HOTP for sending OTP via email
+    totp = pyotp.TOTP(otp_secret)
+    otp = totp.now()
     send_otp_email(recipient_email, otp)
 
     return jsonify({'message': 'OTP sent successfully'}), 200
@@ -57,17 +63,15 @@ def verify_otp():
     if not recipient_email or not user_input_otp:
         return jsonify({'error': 'Email or OTP is missing'}), 400
 
-    # Generate the OTP using pyotp with counter-based HOTP
-    otp_secret = pyotp.random_base32()
-    hotp = pyotp.HOTP(otp_secret)
-    otp_counter = 0  # Set the initial counter value
+    # Check if OTP secret for the email exists
+    if recipient_email not in otp_secrets:
+        return jsonify({'error': 'OTP secret not found. Send OTP first using /send-otp.'}), 400
 
-    # For verification, manually check multiple OTPs in the window
-    window = 3  # Adjust the window size based on your requirements
+    otp_secret = otp_secrets[recipient_email]
+    totp = pyotp.TOTP(otp_secret)
 
-    for i in range(otp_counter, otp_counter + window + 1):
-        if hotp.verify(user_input_otp, i):
-            return jsonify({'message': 'OTP verification successful'}), 200
+    if totp.verify(user_input_otp):
+        return jsonify({'message': 'OTP verification successful'}), 200
 
     return jsonify({'error': 'OTP verification failed'}), 401
 
